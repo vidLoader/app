fe22738de7fb0deda91e5f1d394d7997f73d192f
vidLoader <88811219+vidLoader@users.noreply.github.com>
2021-08-12 07:40:53 +0500

    Delete main.js

--- app/main.js
+++
@@ -1,9 +0,0 @@
-document.querySelectorAll("a")[5].addEventListener("click", increaseCount);
-var click_count = 0;
-
-function increaseCount(){
-    click_count += 1;
-    var download_string = "Downloads: " + click_count;
-   document.getElementById("download-count").innerHTML = download_string;
-}
-
